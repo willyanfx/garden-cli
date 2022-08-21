bd62a048569c519c1a8b3c300be4c116ce98fab0
willyanfx <willyan.fx@gmail.com>
2022-08-21 09:53:15 -0700

    create a temporaty file

--- src/write.rs
+++ src/write.rs
@@ -1,11 +1,27 @@
 use color_eyre::{eyre::Context, Result};
-use edit::edit;
-use std::path::PathBuf;
+use edit::{edit, edit_file, Builder};
+use std::{
+    io::{Read, Write},
+    path::PathBuf,
+};
+
+const TEMPLATE: &[u8; 2] = b"# ";
 
 pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
-    dbg!(garden_path, title);
-    let template = "# ";
-    let content_from_user = edit::edit(template).wrap_err("unable to read writing")?;
-    dbg!(content_from_user);
+    let (mut file, filepath) = Builder::new()
+        .suffix(".md")
+        .rand_bytes(5)
+        .tempfile_in(&garden_path)
+        .wrap_err("Failed to create wip file")?
+        .keep()
+        .wrap_err("Failed to keep tempfile")?;
+    file.write_all(TEMPLATE)?;
+
+    edit_file(filepath)?;
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents);
+
+    dbg!(contents);
     todo!()
 }
