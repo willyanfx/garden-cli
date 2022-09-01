6a3ff1e4ea0cab89a32192efe8b845433cd5bfcd
willyanfx <willyan.fx@gmail.com>
2022-09-01 09:03:52 -0700

    add extra fn

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use color_eyre::eyre::Result;
+use color_eyre::eyre::*;
+use directories::UserDirs;
+use lib_garden::write::write;
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
+#[structopt(name = "garden")]
 struct Opt {
+    #[structopt(parse(from_os_str, short = "p", long, env))]
+    garden_path: Option<PathBuf>,
+
     #[structopt(subcommand)]
     cmd: Command,
 }
@@ -15,12 +22,23 @@ enum Command {
     },
 }
 
+fn get_default_dir() -> Result<PathBuf> {
+    let user_dirs = UserDirs::new().ok_or_else(|| eyre!("Could not find home director"))?;
+    Ok(user_dirs.home_dir().join(".garden"))
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
     let opt = Opt::from_args();
     // debug
     dbg!(&opt);
+
+    let garden_path = match opt.garden_path {
+        Some(pathbuf) => Ok(pathbuf),
+        None => get_default_dir().wrap_err("`garden_path` was not supplied"),
+    };
+
     match opt.cmd {
-        Command::Write { title: _ } => todo!(),
+        Command::Write { title } => write(title),
     }
 }
--- src/write.rs
+++ src/write.rs
@@ -1,11 +1,38 @@
 use color_eyre::{eyre::Context, Result};
-use edit::{edit, edit_file, Builder};
+use edit::{edit_file, Builder};
 use std::{
-    io::{Read, Write},
+    io::{Read, Seek, SeekFrom, Write},
     path::PathBuf,
 };
 
 const TEMPLATE: &[u8; 2] = b"# ";
+fn ask_for_filename() -> Result<String> {
+    rprompt::prompt_reply_stderr(
+        "\
+    Enter filename
+    >",
+    )
+    .wrap_err("Failed to get filename")
+    .map(|title| slug::slugify(title))
+}
+
+fn confirm_filename(raw_title: &str) -> Result<String> {
+    loop {
+        let result = rprompt::prompt_reply_stderr(&format!(
+            r"\ 
+        current title: `{}`
+        Do you want a different title? (y/N): ",
+            raw_title,
+        ))
+        .wrap_err("Failed to get input for y/n question")?;
+
+        match result.as_str() {
+            "y" | "Y" => break ask_for_filename(),
+            "n" | "N" | "" => break Ok(slug::slugify(raw_title)),
+            _ => {}
+        }
+    }
+}
 
 pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
     let (mut file, filepath) = Builder::new()
@@ -20,8 +47,19 @@ pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
     edit_file(filepath)?;
 
     let mut contents = String::new();
-    file.read_to_string(&mut contents);
+    file.seek(SeekFrom::Start(0))?;
+    file.read_to_string(&mut contents)?;
 
-    dbg!(contents);
+    let document_title = title.or_else(|| {
+        contents
+            .lines()
+            .find(|v| v.starts_with("# "))
+            .map(|maybe_line| maybe_line.trim_start_matches("# ").to_string())
+    });
+
+    let filename = match document_title {
+        Some(raw_title) => confirm_filename(&raw_title),
+        None => ask_for_filename(),
+    };
     todo!()
 }
